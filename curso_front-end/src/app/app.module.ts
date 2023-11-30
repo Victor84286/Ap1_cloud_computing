15ddec85e8125420d2c06205bef6ae138f9072f1
Victor Hugo Rocha Aguiar Pinto <victorhrapinto@gmail.com>
2023-11-30 13:04:04 -0300

    envio fial ap2

--- curso_front-end/src/app/app.module.ts
+++ curso_front-end/src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './pages/home/home.component';
 import { DetailComponent } from './pages/detail/detail.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { CursoComponent } from './components/curso/curso.component';
 import { AlunoComponent } from './components/aluno/aluno.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
